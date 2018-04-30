816e3136b7630ad19bf22e57aa4de40f7c7b16fc
Julien Chiquet <julien.chiquet@gmail.com>
2018-04-30 14:16:48 +0200

    version 2?6 to be submitted on CRAN

---
+++ R/init.R
@@ -0,0 +1,11 @@
+.onAttach <- function(...) {
+  welcome <- paste(""                                                      ,
+                   "------------------------------------------------------",
+                   "  'quadrupen' package version 0.2-6                   ",
+                   ""                                                      ,
+                   " Dev version on https://github.com/jchiquet/quadrupen ",
+                   "------------------------------------------------------",
+                   sep = "\n")
+  packageStartupMessage(welcome)
+}
+
